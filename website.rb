ceae883c155be7e92e209ed8e7a87ce10a424551
Addler Pierre-Louis <jhunterswagg365@gmail.com>
2013-07-12 20:16:30 -0400

    First two pages of website basically completed

--- website.rb
+++ website.rb
@@ -1,1 +1,20 @@
-website.rb
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb :index
+
+end
+
+
+get '/HomePage' do
+	"Wassup world"
+end
+
+get '/instructions' do
+	erb :instructions
+end
+
+get '/dragon game' do
+	erb :dragonGame
+end
